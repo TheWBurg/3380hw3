2b1201bf130c7df750c9b6ba585ca849fa412ceb
William Berquist <uncreativeusername5321@gmail.com>
2021-11-29 03:34:20 -0600

    Fixed ticketStatus to work for connecting flights

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ app.post('/searchFlight', async function (req, res) {
         console.log(err.message);
         res.json(err.message)
     }
-    console.log(q.rows);
+    console.log(q.rows + "hello");
     //var r = JSON.stringify(q.rows)
 
     // send stuff back to frontend
@@ -311,19 +311,32 @@ app.post('/getTicketDetails', async function (req, res) {
     try {
         q = await pool.query (
             `SELECT
-            boarding_pass.ticket_no, boarding_pass.flight_id, boarding_pass.class_type, boarding_pass.num_bags, 
+            boarding_pass.ticket_no, boarding_pass.flight_id, boarding_pass.flight_id_2, boarding_pass.class_type, boarding_pass.num_bags, 
+
             payment.final_price, payment.is_cancelled, 
-            flight.departure_airport_id, flight.arrival_airport_id, flight.sch_departure_time, flight.sch_arrival_time, 
-            flight.status, flight.departure_gate_code, flight.arrival_gate_code,
-            depAirport.airport_name, depAirport.airport_city, 
-			arAirport.airport_name, arAirport.airport_city
+
+            f1.departure_airport_id AS f1_departure_airport_id, f1.arrival_airport_id AS f1_arrival_airport_id, 
+            f2.departure_airport_id AS f2_departure_airport_id, f2.arrival_airport_id AS f2_arrival_airport_id, 
+
+            f1.sch_departure_time AS f1_sch_departure_time, f1.sch_arrival_time AS f1_sch_arrival_time, 
+            f1.status AS f1_status, f1.departure_gate_code AS f1_departure_gate_code, f1.arrival_gate_code AS f1_arrival_gate_code,
+            f2.departure_airport_id AS f2_departure_airport_id, f2.arrival_airport_id AS f2_arrival_airport_id, 
+
+            f2.sch_departure_time AS f2_sch_departure_time, f2.sch_arrival_time AS f2_sch_arrival_time, 
+            f2.status AS f2_status, f2.departure_gate_code AS f2_departure_gate_code, f2.arrival_gate_code AS f2_arrival_gate_code,
+            depAirport.airport_name AS f1_dep_airport_name, depAirport.airport_city AS f1_dep_airport_name, 
+
+			arAirport.airport_name AS f1_arr_airport_name, arAirport.airport_city AS f1_arr_airport_name,
+            arFinalAirport.airport_name AS f2_arr_airport_name, arFinalAirport.airport_city AS f2_arr_airport_name
 
             FROM boarding_pass
 
             INNER JOIN payment ON boarding_pass.ticket_no = payment.ticket_no
-            INNER JOIN flight ON boarding_pass.flight_id = flight.flight_id
-            INNER JOIN airport depAirport ON flight.departure_airport_id = depAirport.airport_id 
-            INNER JOIN airport arAirport ON flight.arrival_airport_id = arAirport.airport_id
+            INNER JOIN flight f1 ON boarding_pass.flight_id = f1.flight_id
+            INNER JOIN flight f2 ON boarding_pass.flight_id_2 = f2.flight_id
+            INNER JOIN airport depAirport ON f1.departure_airport_id = depAirport.airport_id 
+            INNER JOIN airport arAirport ON f1.arrival_airport_id = arAirport.airport_id
+            INNER JOIN airport arFinalAirport ON f2.arrival_airport_id = arFinalAirport.airport_id
 
             WHERE payment.ticket_no = ${t.ticket_no} AND payment.ssn = '${t.ssn}';`
         );
